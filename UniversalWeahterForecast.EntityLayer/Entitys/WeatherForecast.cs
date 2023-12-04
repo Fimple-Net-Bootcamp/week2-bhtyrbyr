3ba456fa506e29e981e83e796d7f29df6862f26f
bhtyrbyr <bhtyr.byr@gmail.com>
2023-12-04 21:49:22 +0300

    Entity layer is created.

---
+++ UniversalWeahterForecast.EntityLayer/Entitys/WeatherForecast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalWeahterForecast.EntityLayer.Entitys
+{
+    internal class WeatherForecast
+    {
+        internal int Id { get; set; }
+        internal int CelestialBodyId { get; set; }
+        internal WeatherTypes WeatherType { get; set; }
+        internal DateTime? WeatherTime { get; set; }
+    }
+}
