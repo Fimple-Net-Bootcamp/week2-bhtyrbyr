1b7ad8f586e0917ae7dc7135047e63a1a5f74b74
bhtyrbyr <bhtyr.byr@gmail.com>
2023-12-06 22:09:36 +0300

    Updated get methods in the celestial body controller. Filtering feature has been added.

--- UniversalWeahterForecast.WebApi/Controllers/CelestalBodyController.cs
+++ UniversalWeahterForecast.WebApi/Controllers/CelestalBodyController.cs
@@ -21,23 +21,7 @@ namespace UniversalWeahterForecast.WebApi.Controllers
         {
             return Ok(_service.TGetList(filter));
         }
-        /*
-        [HttpGet("Planets")]
-        public IActionResult GetPlanets()
-        {
-            var list = _service.TGetList();
-            var planetList = list = list.Where(x => x.CelestalBodyType == "Planet").ToList();
-            return Ok(list);
-        }
 
-        [HttpGet("Satellites")]
-        public IActionResult GetSatellites()
-        {
-            var list = _service.TGetList();
-            var satellitesList = list = list.Where(x => x.CelestalBodyType == "Satellite").ToList();
-            return Ok(list);
-        }
-        */
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
